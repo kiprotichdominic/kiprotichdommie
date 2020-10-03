22607b805ef6bac8b0c83fd940f7939e00401c74
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-10-03 13:46:56 +0300

    add testimonial db

--- landingpage/models.py
+++ landingpage/models.py
@@ -15,7 +15,7 @@ class PortfolioProject(models.Model):
 
     class Meta:
         verbose_name = 'Portfolio Project'
-        verbose_name_plural = 'Portfolio Project'
+        verbose_name_plural = 'Portfolio Projects'
 
     def save(self, *args, **kwargs):
         super(PortfolioProject, self).save(*args, **kwargs)
@@ -26,3 +26,27 @@ class PortfolioProject(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.project_image.path)
+
+
+class Testimonial(models.Model):
+    name = models.CharField(max_length=250)
+    testimonial = models.TextField()
+    career = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Testimonial_image')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Testimonial'
+        verbose_name_plural = 'Testimonials'
+
+    def save(self, *args, **kwargs):
+        super(Testimonial, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
