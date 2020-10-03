a76b25fa0367f263a73e3eb7b7e2cfe2c8fe1457
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-10-03 13:05:29 +0300

    add projects from db

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, DeleteView
 
+from .models import PortfolioProject
 
-class LandingPageView(TemplateView):
-    template_name = 'landingpage/index.html'
+
+class LandingPageView(ListView):
+    model=PortfolioProject
+    template_name='landingpage/index.html'
