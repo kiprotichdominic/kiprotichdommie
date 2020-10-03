e5b52dab884bbfc758de32c176db116dc7567ee3
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-10-03 11:17:36 +0300

    add portfolio projects to db

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
+
+
+class PortfolioProject(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=300)
+    link = models.URLField(max_length=200)
+    project_image = models.ImageField(upload_to='Portfolio_projects')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Portfolio Project'
+        verbose_name_plural = 'Portfolio Project'
+
+    def save(self, *args, **kwargs):
+        super(PortfolioProject, self).save(*args, **kwargs)
+
+        img = Image.open(self.project_image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.project_image.path)
--- users/models.py
+++ users/models.py
@@ -10,23 +10,26 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
-    
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(get_user_model(),on_delete=models.CASCADE)
-    profile_image = models.ImageField(default='default.png',upload_to='profile_pics')
-    
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    profile_image = models.ImageField(
+        default='dommie.jpeg', upload_to='profile_pics')
+
     def __str__(self):
         return f'{self.user.username} Profile'
 
     class Meta:
         verbose_name = 'Profile'
         verbose_name_plural = 'Profiles'
+
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.profile_image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size = (300,300)
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.profile_image.path)
