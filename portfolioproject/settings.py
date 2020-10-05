20eab0c5bd82a05f1a2547bae085d6ce1286ffae
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-10-05 13:23:07 +0300

    add potgress db

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'portfolioproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'portfolio',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin2020',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': '123456@#',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
